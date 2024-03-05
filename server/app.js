95b0efc3d5d0814542ca60a3dad04d1f43d6fcd6
Bryce Hiraoka <bryce.hiraoka@gmail.com>
2024-03-05 14:59:49 -0500

    finishe oauth logout

--- server/app.js
+++ server/app.js
@@ -3,7 +3,8 @@ const session = require("express-session");
 const passport = require("passport");
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(session({secret: 'cats '}));
+
+app.use(session({secret: 'cats', resave: false, saveUninitialized: true}));
 app.use(passport.initialize());
 app.use(passport.session());
 require('./auth')
@@ -12,12 +13,6 @@ function isLoggedIn(req, res, next) {
   req.user ? next() : res.sendStatus(401);
 }
 
-
-app.get("/api", (req, res) => {
-    res.send({ message: "Hello from Express!" });
-  }); 
-  app.listen(port, () => console.log(`Listening on port ${port}`));
-
 app.get('/', (req, res) => {
   res.send('<a href="/auth/google">Authenticate </a>');
 });
@@ -25,20 +20,30 @@ app.get('/', (req, res) => {
 app.get('/auth/google/callback',
   passport.authenticate('google', {
     successRedirect: '/protected',
-    failureRedirect: '/auth/failure',
+    failureRedirect: '/auth/google/failure',
   })
 );
 
 app.get('/protected', isLoggedIn, (req, res) => {
-  res.send("Hello World")
+  res.setHeader('Content-Type', 'text/html');
+  res.write(`Hello ${req.user.displayName}`);
+  res.write('<a href="/logout"> Logout </a>');
+  res.end();
 });
+
+app.get('/logout', (req, res) => {
+  req.logout(function(err) {
+    if(err) { return next (err); }
+  });
+  res.send('Logged Out');
+})
 
 app.get('auth/google/failure', (req, res) => {
   res.send('something went wrong..');
 });
 
-
-
 app.get('/auth/google', passport.authenticate('google', 
   { scope: ['email', 'profile']
 }));
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
