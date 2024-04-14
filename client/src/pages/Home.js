cc362cce9931fc7c7a821035b6aa0759d1cf1e18
Theo <theodorechen03@gmail.com>
2024-04-13 20:09:31 -0400

    Added floating JSA logo on top of carousel

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,16 +2,29 @@ import React, { Component } from 'react';
 import logo from '../assets/jsa.svg';
 
 import HomeCarousel from '../components/HomeCarousel';
+import Image from 'react-bootstrap/Image';
 
 class Home extends Component {
     render() {
         return (
             <div>
-                <HomeCarousel />
+                <div style={{position: 'relative',                            
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',}}>
+                    <HomeCarousel />
+                    <Image src={logo} rounded style={{
+                            position: 'absolute', 
+                            width: '30%', 
+                            height: '30%',
+                            transform: 'translateY(-60%)'
+                        }} 
+                    />
+                    </div>
                 <header className="App-header">
                     <img className="App-logo" src={logo} alt="logo" />
                     <h1 className="App-title">Welcome to JSA</h1>
-                </header>
+                </header>    
             </div>
         );
     }
